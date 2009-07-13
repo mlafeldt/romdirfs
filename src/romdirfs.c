3fc734c1a451f0cfb9bd68f3d121bf2b615564f9
Mathias Lafeldt <misfire@debugon.org>
2009-07-13 15:27:54 +0200

    Some reorganization.

--- src/romdirfs.c
+++ src/romdirfs.c
@@ -9,46 +9,50 @@
 #include <error.h>
 
 /* Size of one ROMDIR entry */
-#define ROM_HDR_SIZE 16
+#define ROMENT_SIZE	16
 
-/* Structure of a ROMDIR entry */
-typedef struct _romhdr {
+/* ROMDIR entry */
+typedef struct _roment {
 	char		name[10];
 	u_int16_t	extinfo_size;
 	u_int32_t	size;
-} romhdr_t;
-
-typedef struct _roment {
-	romhdr_t	header;
-	u_int32_t	offset;
-	u_int8_t	*data;
-
-	STAILQ_ENTRY(_roment) node;
 } roment_t;
 
-typedef STAILQ_HEAD(_roment_queue, _roment) roment_queue_t;
+/* ROMDIR file information */
+typedef struct _romfile {
+	char		name[10];
+	u_int16_t	extinfo_size;
+	u_int32_t	size;
+	u_int32_t	offset;
 
-static void __show_entry(const roment_t *entry)
+	STAILQ_ENTRY(_romfile) node;
+} romfile_t;
+
+/* Queue to hold multiple ROMDIR files */
+typedef STAILQ_HEAD(_romfile_queue, _romfile) romfile_queue_t;
+
+
+static void __show_romfile(const romfile_t *file)
 {
-	printf("%-10s %04x %08x %08x-%08x\n", entry->header.name,
-		entry->header.extinfo_size, entry->header.size,
-		entry->offset, entry->offset + entry->header.size);
+	printf("%-10s %04x %08x %08x-%08x\n", file->name,
+		file->extinfo_size, file->size,
+		file->offset, file->offset + file->size);
 }
 
-/* Parse file for rom entries. */
-static int __parse_romfile(int fd, roment_queue_t *queue)
+/* Parse file for ROMDIR entries. */
+static int __parse_file(int fd, romfile_queue_t *queue)
 {
-	int reset_found = 0;
-	romhdr_t header;
-	roment_t *entry = NULL;
+	roment_t entry;
+	romfile_t *file = NULL;
 	u_int32_t offset = 0;
+	int reset_found = 0;
 
 	STAILQ_INIT(queue);
 
 	/* find RESET module */
 	lseek(fd, 0, SEEK_SET);
-	while (read(fd, &header, ROM_HDR_SIZE) == ROM_HDR_SIZE) {
-		if (!strcmp(header.name, "RESET")) {
+	while (read(fd, &entry, ROMENT_SIZE) == ROMENT_SIZE) {
+		if (!strcmp(entry.name, "RESET")) {
 			reset_found = 1;
 			break;
 		}
@@ -60,18 +64,18 @@ static int __parse_romfile(int fd, roment_queue_t *queue)
 	}
 
 	do {
-		/* add rom entry to queue */
-		entry = (roment_t*)malloc(sizeof(roment_t));
-		memcpy(&entry->header, &header, ROM_HDR_SIZE);
-		entry->offset = offset;
-		STAILQ_INSERT_TAIL(queue, entry, node);
+		/* add ROMDIR entry to queue */
+		file = (romfile_t*)malloc(sizeof(romfile_t));
+		memcpy(file, &entry, ROMENT_SIZE);
+		file->offset = offset;
+		STAILQ_INSERT_TAIL(queue, file, node);
 
 		/* offset must be aligned to 16 bytes */
-		if (!(header.size % 0x10))
-			offset += header.size;
+		if (!(entry.size % 0x10))
+			offset += entry.size;
 		else
-			offset += (header.size + 0x10) & 0xfffffff0;
-	} while (read(fd, &header, ROM_HDR_SIZE) == ROM_HDR_SIZE && header.name[0]);
+			offset += (entry.size + 0x10) & 0xfffffff0;
+	} while (read(fd, &entry, ROMENT_SIZE) == ROMENT_SIZE && entry.name[0]);
 
 	return 0;
 }
@@ -79,8 +83,8 @@ static int __parse_romfile(int fd, roment_queue_t *queue)
 int main(int argc, char *argv[])
 {
 	int fd;
-	roment_queue_t queue;
-	roment_t *entry;
+	romfile_queue_t queue;
+	romfile_t *file;
 
 	if (argc != 2)
 		return -1;
@@ -91,12 +95,12 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	__parse_romfile(fd, &queue);
+	__parse_file(fd, &queue);
 
-	STAILQ_FOREACH(entry, &queue, node) {
-		__show_entry(entry);
-		STAILQ_REMOVE(&queue, entry, _roment, node);
-		free(entry);
+	STAILQ_FOREACH(file, &queue, node) {
+		__show_romfile(file);
+		STAILQ_REMOVE(&queue, file, _romfile, node);
+		free(file);
 	}
 
 	close(fd);
